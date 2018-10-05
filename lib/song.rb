64bc4afc0512ea9e30ae8d76d9d13c7ac05afca8
Gina Fedder <ginafedder@yahoo.com>
2018-10-05 16:41:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     #self.new_by_name(song_name).tap{|song| song.artist_name = artist_name}
     #song = self.new(song_name)
     song = Song.new(song_name)
-    
+    song = song.new(artist_name)
     song
   end 
   
