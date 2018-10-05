f7c14045cdffc57dc4a3e9ebdde453d49cb48c83
Gina Fedder <ginafedder@yahoo.com>
2018-10-05 17:15:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,6 @@ class Song
   
   def self.new_by_filename(file_name)
     artist_name, song_name, genre = file_name.split(" - ")
-    #self.new_by_name(song_name).tap{|song| song.artist_name = artist_name}
     song = self.new(song_name)
     song.artist_name = artist_name
     song
