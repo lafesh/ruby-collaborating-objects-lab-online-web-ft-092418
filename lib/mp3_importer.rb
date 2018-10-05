32f1da73a77f6539242a65ed7bbe5f5000a708d6
Gina Fedder <ginafedder@yahoo.com>
2018-10-05 15:32:24 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class MP3Importer
+  
+  attr_accessor :path
+  
+  def initialize(path)
+    self.path = path 
+    @files = []
+  end 
+  
+  def files
+    @files << self.path
+    @files
+  end 
+  
+  def import 
+    files.each {|file| Song.new_by_filename(file)}
+    
+  end 
+  
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  def initialize(name)
+    self.name = name 
+  end 
+  
+  def self.new_by_name(name)
+    self.new.tap{|song| song.name = name}
+  end 
+  
+  def self.new_by_filename(file_name)
+    song_name, artist_name = file_name.split(" - ")
+    self.new_by_name(song_name).tap{|song| song.artist_name = artist_name}
+    #song = MP3Importer.new.song_name
+  end 
+  
+  
+end 
